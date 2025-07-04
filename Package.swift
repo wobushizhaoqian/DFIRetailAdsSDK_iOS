4ba1c71976557558d39453bc4c5ab20121ab6576
zqzhao <zqzhao@dairy-farm.com.cn>
2025-07-04 17:29:01 +0800

    0.0.8

--- Package.swift
+++ Package.swift
@@ -10,12 +10,12 @@ let package = Package(
             targets: ["DFIRetailAdsSDK_iOS"])
     ],
     dependencies: [
-    .package(url: "https://github.com/wobushizhaoqian/DFIRetailAdsSDK_iOS.git", from: "0.0.7") // 确保URL和名称正确
+    .package(url: "https://github.com/wobushizhaoqian/DFIRetailAdsSDK_iOS.git", from: "0.0.8") // 确保URL和名称正确
     ],
     targets: [
         .binaryTarget(
             name: "DFIRetailAdsSDK_iOS",
-            url: "https://github.com/wobushizhaoqian/DFIRetailAdsSDK_iOS/releases/download/0.0.7/DFIRetailAdsSDK_iOS.xcframework.zip",
+            url: "https://github.com/wobushizhaoqian/DFIRetailAdsSDK_iOS/releases/download/0.0.8/DFIRetailAdsSDK_iOS.xcframework.zip",
             checksum: "85f4fcad4622eeb4347888c6d92e8ff4fbd17a6b94fe91fd6b9c7766312f4af8")
     ]
 )
